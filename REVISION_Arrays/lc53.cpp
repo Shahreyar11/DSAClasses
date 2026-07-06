808765686bcde76155de5113e392adba9e759d75
Shahreyar11 <shahreyar551@gmail.com>
2026-07-06 12:35:34 +0530

    stacks more questions

---
+++ REVISION_Arrays/lc53.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int n = nums.size();
+        int bestending = nums[0];
+        int ans = nums[0];
+        for(int i = 1; i<n; i++){
+            int v1 = bestending + nums[i];
+            int v2 = nums[i];
+            bestending = max(v1,v2);
+            ans = max(ans,bestending);
+        }
+        return ans;
+    }
+};
