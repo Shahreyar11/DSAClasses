ce96928f44c521152e0779e7498bee74cf1ee00e
Shahreyar11 <shahreyar551@gmail.com>
2026-07-11 20:07:06 +0530

    array rev

---
+++ Queue/lc1700.cpp
@@ -0,0 +1,35 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+class Solution {
+public:
+    int countStudents(vector<int>& students, vector<int>& sandwiches) {
+
+        queue<int> q;
+        stack<int> st;
+
+        for (int x : students)
+            q.push(x);
+
+        for (int i = sandwiches.size() - 1; i >= 0; i--)
+            st.push(sandwiches[i]);
+
+        int rotations = 0;
+
+        while (!q.empty() && rotations < q.size()) {
+
+            if (q.front() == st.top()) {
+                q.pop();
+                st.pop();
+                rotations = 0;
+            } else {
+                q.push(q.front());
+                q.pop();
+                rotations++;
+            }
+        }
+
+        return q.size();
+    }
+};
