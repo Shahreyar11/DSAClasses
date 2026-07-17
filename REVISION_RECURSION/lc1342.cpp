51fc75ec0cff94a1fba758fbd38a12255dd482c6
Shahreyar11 <shahreyar551@gmail.com>
2026-07-17 10:50:13 +0530

    recursion solved

---
+++ REVISION_RECURSION/lc1342.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int numberOfSteps(int num) {
+        if(num == 0){
+            return 0;
+        }
+        else if(num % 2 == 0){
+            num = num/2;
+        }
+        else{
+            num = num -1;
+        }
+        return (1+(numberOfSteps(num)));
+    }
+};
